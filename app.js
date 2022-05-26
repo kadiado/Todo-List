9dc3d9b7da77cc1ff0a5d7ee230deca6224ec209
kadiado <marcothulio2@gmail.com>
2022-05-26 09:51:50 -0300

    habilitando opção de deletar to-do

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const formAddTodo = document.querySelector(".list-group");
-const todoContainer = document.querySelector(".form-add-todo");
+const formAddTodo = document.querySelector(".form-add-todo");
+const todosContainer = document.querySelector(".todos-container");
+const inputSearchTodo = document.querySelector('.form-search input')
 
-todoContainer.addEventListener("submit", event => {
+formAddTodo.addEventListener("submit", event => {
   event.preventDefault();
 
   const contentLI = event.target.add.value.trim();
-  const input = event.target.add;
+  
 
   if (contentLI.length) {
-    formAddTodo.innerHTML += `
+    todosContainer.innerHTML += `
     <li class="list-group-item d-flex justify-content-between align-items-center">
     <span>${contentLI}</span>
     <i class="far fa-trash-alt delete"></i>
@@ -17,3 +18,34 @@ todoContainer.addEventListener("submit", event => {
 
   event.target.reset();
 });
+
+todosContainer.addEventListener('click', event => {
+  const clickedElement = event.target
+  const classListOfTrash = Array.from(clickedElement.classList).includes('delete')
+
+  if (classListOfTrash) {
+    console.log(clickedElement.parentElement.remove());
+  }
+});
+
+
+inputSearchTodo.addEventListener('input', event => {
+  const inputValue = event.target.value.trim().toLowerCase();
+  
+  Array.from(todosContainer.children)
+  .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+  .forEach(todo => {
+    todo.classList.remove('d-flex')
+    todo.classList.add('hidden')
+  })
+
+  Array.from(todosContainer.children)
+  .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+  .forEach(todo => {
+    todo.classList.remove('hidden')
+    todo.classList.add('d-flex')
+  })
+
+ 
+
+})
