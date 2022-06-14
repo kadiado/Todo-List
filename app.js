9abdc08fd994ff2ceff2b066e195191ae40f6bc2
kadiado <marcothulio2@gmail.com>
2022-06-14 13:01:54 -0300

    add

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const formAddTodo = document.querySelector(".form-add-todo");
 const todosContainer = document.querySelector(".todos-container");
-const inputSearchTodo = document.querySelector('.form-search input')
+const inputSearchTodo = document.querySelector(".form-search input");
 
 formAddTodo.addEventListener("submit", event => {
   event.preventDefault();
 
   const contentLI = event.target.add.value.trim();
-  
 
   if (contentLI.length) {
     todosContainer.innerHTML += `
@@ -19,33 +18,29 @@ formAddTodo.addEventListener("submit", event => {
   event.target.reset();
 });
 
-todosContainer.addEventListener('click', event => {
-  const clickedElement = event.target
-  const classListOfTrash = Array.from(clickedElement.classList).includes('delete')
+todosContainer.addEventListener("click", event => {
+  const clickedElement = event.target;
+  const classListOfTrash = Array.from(clickedElement.classList).includes("delete");
 
   if (classListOfTrash) {
     console.log(clickedElement.parentElement.remove());
   }
 });
 
-
-inputSearchTodo.addEventListener('input', event => {
+inputSearchTodo.addEventListener("input", event => {
   const inputValue = event.target.value.trim().toLowerCase();
-  
-  Array.from(todosContainer.children)
-  .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
-  .forEach(todo => {
-    todo.classList.remove('d-flex')
-    todo.classList.add('hidden')
-  })
 
   Array.from(todosContainer.children)
-  .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
-  .forEach(todo => {
-    todo.classList.remove('hidden')
-    todo.classList.add('d-flex')
-  })
+    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+      todo.classList.remove("d-flex");
+      todo.classList.add("hidden");
+    });
 
- 
-
-})
+  Array.from(todosContainer.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+      todo.classList.remove("hidden");
+      todo.classList.add("d-flex");
+    });
+});
